c50fdfa67a931df131da2420b149257c73f7cb19
Tran Tam <tranminhtam.10192@gmail.com>
2016-12-02 01:12:38 +0700

    rename core.py to subupy_serial.py, subupy.py to subupy_command.py

--- subupy_serial.py
+++ subupy_serial.py
@@ -9,6 +9,7 @@ import binascii
 import time
 import re
 import ast
+import os, sys, glob
 
 #---- CONSTANT
 
@@ -80,6 +81,35 @@ class SubUpySerial():
 		return response
 
 class SubUpyUtility():
+    @staticmethod
+    def GetPorts():
+        """ Lists serial port names
+
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
+
 	@staticmethod
 	def ListFile(subupy_serial):
 		subupy_serial.SendCmd("import os")
