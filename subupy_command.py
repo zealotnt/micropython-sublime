9536dc85ad9db9a6831dc5e6c210d631994f74f0
Tran Tam <tranminhtam.10192@gmail.com>
2016-12-03 22:47:34 +0700

    add UI responsiveness windows + status bar message
    + open file: ok/ no file to open
    + delete file: ok/ no file to open
    + save current file: success (not fail case yet)
    + connect port: open success (not fail case yet)

--- subupy_command.py
+++ subupy_command.py
@@ -10,15 +10,43 @@ from subupy_serial import *
 settings = sublime.load_settings('subupy.sublime-settings')
 
 #Select and save port settings
-class SubUpyConnectPort(sublime_plugin.ApplicationCommand):
+def HtmlMessage(text, color="gray"):
+    return """
+            <body id=show-scope>
+                <style>
+                    p {
+                        margin-top: 0;
+                        margin-bottom: 0;
+                        color: %s
+                    }
+                    a {
+                        font-family: sans-serif;
+                        font-size: 1.05rem;
+                    }
+                </style>
+                <p>%s</p>
+            </body>
+        """ % (color, text)
+
+def PopUpMessage(view, text, color="gray"):
+    scope = view.scope_name(view.sel()[-1].b)
+    html = HtmlMessage(text, color)
+    view.show_popup(html, on_navigate=lambda x: copy(view, x))
+
+class SubUpyConnectPort(sublime_plugin.WindowCommand):
     def selected_callback(self, selected_index):
         if settings.has("port"):
             print('Closing %s' % settings.get("port", None))
             # TODO: Close the current opened com port
-        settings.set("port", avail_ports[selected_index])
+        port = avail_ports[selected_index]
+        settings.set("port", port)
         sublime.save_settings('subupy.sublime-settings')
         global gserial
-        gserial = SubUpySerial(avail_ports[selected_index], 115200)
+        gserial = SubUpySerial(port, 115200)
+
+        # Promt message to user
+        sublime.status_message("%s opened successfully !" % port)
+        PopUpMessage(self.window.active_view(), "%s opened successfully !" % port)
 
     def run(self):
         global avail_ports
@@ -31,27 +59,31 @@ class SubUpySaveCurrentFile(sublime_plugin.WindowCommand):
         body = self.window.active_view().substr(sublime.Region(0, self.window.active_view().size()))
         file_name = os.path.basename(self.window.active_view().file_name())
         SubUpyUtility.WriteFile(gserial, file_name, body)
-        print("Save done")
-        # TODO: pop up save success message
+        sublime.status_message("%s saved successfully !" % file_name)
+        PopUpMessage(self.window.active_view(), "%s saved successfully !" % file_name)
 
-class SubUpyDeleteFile(sublime_plugin.ApplicationCommand):
+class SubUpyDeleteFile(sublime_plugin.WindowCommand):
     def selected_callback(self, selected_index):
         if selected_index == -1:
             return
-        SubUpyUtility.RemoveFile(gserial, files[selected_index])
-        print("File %s deleted" % files[selected_index])
-        # TODO: print a delete success message
+        deleted_file = files[selected_index]
+        SubUpyUtility.RemoveFile(gserial, deleted_file)
+        sublime.status_message("File %s deleted" % deleted_file)
+        print(self)
+        PopUpMessage(self.window.active_view(), "File %s deleted" % deleted_file)
 
     def run(self):
+        print(self)
         global files
         files = SubUpyUtility.ListFile(gserial)
         if len(files) == 0:
-            # TODO: Pop up a error message box to show error
-            print("No more file to delete")
+            # Pop up a error message box to show error
+            sublime.status_message("No more file to delete")
+            PopUpMessage(self.window.active_view(), "No more file to delete", "red")
             return
         sublime.active_window().show_quick_panel(files, self.selected_callback, flags=sublime.KEEP_OPEN_ON_FOCUS_LOST)
 
-class SubUpyOpenFile(sublime_plugin.ApplicationCommand):
+class SubUpyOpenFile(sublime_plugin.WindowCommand):
     def selected_callback(self, selected_index):
         if selected_index == -1:
             return
@@ -61,7 +93,7 @@ class SubUpyOpenFile(sublime_plugin.ApplicationCommand):
         global files
         files = SubUpyUtility.ListFile(gserial)
         if len(files) == 0:
-            # TODO: Pop up a error message box to show error
-            print("No more file to open")
+            sublime.status_message('No more file to open')
+            PopUpMessage(self.window.active_view(), 'No more file to open', "red")
             return
         sublime.active_window().show_quick_panel(files, self.selected_callback, flags=sublime.KEEP_OPEN_ON_FOCUS_LOST)
